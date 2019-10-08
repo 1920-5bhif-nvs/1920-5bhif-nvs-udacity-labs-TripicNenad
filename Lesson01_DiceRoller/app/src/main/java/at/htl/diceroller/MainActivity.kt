717f2f2587d4176cd656623ab7bc2e797e8e92d4
Nenad TRIPIC <tripicnenad@hotmail.com>
2019-10-08 19:40:14 +0200

    Lesson 01 Step 03 added Images and made it dependent on the button

--- Lesson01_DiceRoller/app/src/main/java/at/htl/diceroller/MainActivity.kt
+++ Lesson01_DiceRoller/app/src/main/java/at/htl/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package at.htl.diceroller
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import java.util.*
@@ -15,15 +16,22 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener {
             rollDice()
-            Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
         }
     }
 
     private fun rollDice() {
-        val resultText: TextView = findViewById(R.id.result_text)
+        val randomNumber = Random().nextInt(6) + 1
+        val drawableResource = when (randomNumber) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
 
-        val randomNumber = Random().nextInt(6)+1
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+        diceImage.setImageResource(drawableResource)
 
-        resultText.text = randomNumber.toString()
     }
 }
