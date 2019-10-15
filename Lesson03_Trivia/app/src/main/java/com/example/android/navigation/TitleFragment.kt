203763bdbe0be01a67ae77c130f0486c5109f10e
Nenad TRIPIC <tripicnenad@hotmail.com>
2019-10-15 18:51:07 +0200

    Lesson 03 Step 06: Added the overflow menu to show the aboutFragment

--- Lesson03_Trivia/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ Lesson03_Trivia/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -2,13 +2,12 @@ package com.example.android.navigation
 
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 /**
@@ -24,8 +23,22 @@ class TitleFragment : Fragment() {
         binding.playButton.setOnClickListener(
                 Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
         )
+
+        //telling Android that we have an options menu
+        setHasOptionsMenu(true)
         return binding.root
     }
 
+    //press strg+o to override  methods!!
 
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.overflow_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item!!,
+                view!!.findNavController()) || super.onOptionsItemSelected(item)
+    }
 }
