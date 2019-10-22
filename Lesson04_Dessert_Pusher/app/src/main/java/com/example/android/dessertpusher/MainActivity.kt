80dbc8f21fe3f762670da43d71228453fe11b0b5
Nenad TRIPIC <tripicnenad@hotmail.com>
2019-10-22 17:38:00 +0200

    Lesson 04 Step 03: Setup and teardown. E.g. is the dessertTimer

--- Lesson04_Dessert_Pusher/app/src/main/java/com/example/android/dessertpusher/MainActivity.kt
+++ Lesson04_Dessert_Pusher/app/src/main/java/com/example/android/dessertpusher/MainActivity.kt
@@ -33,6 +33,7 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
 
     private var revenue = 0
     private var dessertsSold = 0
+    private lateinit var dessertTimer: DessertTimer
 
     // Contains all the views
     private lateinit var binding: ActivityMainBinding
@@ -70,6 +71,9 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
         //old way:
         //Log.i("MainActivity","onCreate called")
         Timber.i("onCreate called")
+
+        //Create a DessertTimer
+        dessertTimer = DessertTimer()
 
         // Use Data Binding to get reference to the views
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
@@ -158,6 +162,7 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
         //Log.i("MainActivity","onStart called") //old way
         //with Timber:
         Timber.i("onStart called")
+        dessertTimer.startTimer()
     }
 
     override fun onResume() {
@@ -183,5 +188,6 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
     override fun onStop() {
         super.onStop()
         Timber.i("onStop called")
+        dessertTimer.stopTimer()
     }
 }
