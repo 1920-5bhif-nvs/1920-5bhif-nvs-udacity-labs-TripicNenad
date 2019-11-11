3a4bd562f90dd4d7a0495038906e54ef3531ffce
Nenad TRIPIC <tripicnenad@hotmail.com>
2019-11-11 17:52:35 +0100

    Lesson 07 Step 03: Finished RecyclerView adapter

--- Lesson07_Sleep_Tracker_RecyclerView/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ Lesson07_Sleep_Tracker_RecyclerView/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -57,6 +57,16 @@ class SleepTrackerFragment : Fragment() {
                         this, viewModelFactory
                 ).get(SleepTrackerViewModel::class.java)
         binding.sleepTrackerViewModel = sleepTrackerViewModel
+
+        val adapter = SleepNightAdapter()
+        binding.sleepList.adapter = adapter
+
+        sleepTrackerViewModel.nights.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                adapter.data = it
+            }
+        })
+
         binding.setLifecycleOwner(this)
 
         sleepTrackerViewModel.navigateToSleepQuality.observe(this, Observer { night ->
