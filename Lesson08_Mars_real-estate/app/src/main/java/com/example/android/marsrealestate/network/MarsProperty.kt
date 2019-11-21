4cc159a3f5ef0b9787dabf94bfaa15e7820fa686
Nenad TRIPIC <tripicnenad@hotmail.com>
2019-11-21 10:32:11 +0100

    Lesson 08 Step 09: Added string formating

--- Lesson08_Mars_real-estate/app/src/main/java/com/example/android/marsrealestate/network/MarsProperty.kt
+++ Lesson08_Mars_real-estate/app/src/main/java/com/example/android/marsrealestate/network/MarsProperty.kt
@@ -24,7 +24,10 @@ import kotlinx.android.parcel.Parcelize
 @Parcelize
 data class MarsProperty(
         val id: String,
+        // used to map img_src from the JSON to imgSrcUrl in our class
         @Json(name = "img_src") val imgSrcUrl: String,
         val type: String,
         val price: Double) : Parcelable {
+    val isRental
+        get() = type == "rent"
 }
